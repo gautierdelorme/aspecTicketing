cd3fa711cbef23db5239684a00a71d3f8f385f42
Souleymane Bagyogo <bsouleymane78@gmail.com>
2014-11-21 01:06:44 -0500

    Implémentation des fonction save et load *****pas encore testé***** Ajout des .jar Jsr api pour utiliser son

--- src/User.java
+++ src/User.java
@@ -1,4 +1,19 @@
+import java.io.FileWriter;
 import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonParser;
+import javax.json.JsonStructure;
 
 public class User {
 	private int id;
@@ -66,10 +81,85 @@ public class User {
 	}
 	
 	public void Save() {
+		java.io.File f= new File("userlist.txt");
+		if (!f.exists())
+		{
+			try {
+				f.createNewFile();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+						
+		}
+		FileWriter writer;
+		try {
+			writer = new FileWriter("userlist.txt");
+			JsonGenerator gen = Json.createGenerator(writer);
+			gen.writeStartObject()
+			   .write("username",username)
+			   .write("password",password)
+			   .write("id",id)
+			   .write("credit",credit)
+			   .writeEnd();
+			gen.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
 	public User Load(String username) {
-		return null;
+		java.io.File f= new File("userlist.txt");
+		if (f.exists())
+		{
+			try {
+				JsonReader reader = Json.createReader(new FileReader("userlist.txt"));
+				JsonArray array = reader.readArray();
+				reader.close();
+				int index=0;
+				boolean found=false;
+				while (!found) {
+					for (int i=0;i < array.size();i++)
+					{
+						JsonObject jso= array.getJsonObject(i);
+						String name = jso.getString("username");
+						if (name==username)
+						{
+							index=i;
+							found=true;
+						}
+					}
+					
+				}
+				if (found)
+				{
+					JsonObject jso= array.getJsonObject(index);
+					String passwd=jso.getString("password");
+					int id= Integer.parseInt(jso.getString("id"));
+					double cred= Double.parseDouble(jso.getString("credit"));
+					User us= new User(id, username, passwd, cred);
+					return us;
+					
+				}
+				else 
+				{
+					return null;
+				}
+				
+				
+				
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+		}
+		else {
+			return null;
+		}
+		
 	}
 }
